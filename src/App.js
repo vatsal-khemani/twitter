c74417c93a986cd5f3ce75e21e0e60f82911b938
Vatsal Khemani <43574951+vatsalkhemani@users.noreply.github.com>
2021-06-10 13:10:33 +0530

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
     <h1>HIIIII</h1>
+    {/*Sidebar*/}
+    <Sidebar/>
     </div>
   );
 }
