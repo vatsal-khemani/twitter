53ffba827468823ffbaf6f070faff554e24251f3
Vatsal Khemani <43574951+vatsalkhemani@users.noreply.github.com>
2021-06-13 15:37:41 +0530

    frontend done

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import "./Widgets.css";
+import SearchIcon from "@material-ui/icons/Search";
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+  } from "react-twitter-embed";
+
 
 function Widgets() {
     return (
         <div className="widgets">
-            <h2>Widgets</h2>
+            <div className="widgets__input">
+            <SearchIcon className="widgets__searchIcon" />
+            <input placeholder="Search Twitter" type="text" />
+            </div>
+            <div className="widgets__widgetContainer">
+                <h2>What's happening</h2>
+                <TwitterTweetEmbed tweetId={"1403507144318722051"} />
+                <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="tedx"
+                options={{ height: 400 }}
+                />
+                <TwitterShareButton
+                url={"https://www.ted.com/tedx/events/45395"}
+                options={{ text: "Event", via: "vatsalkhemani" }}
+                />
+
+            </div>
         </div>
     )
 }
